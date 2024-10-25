792913dd1926f2ece0f13dd9a781761ea20a8a9d
billthai12 <billthai12@gmail.com>
2024-10-25 15:36:34 -0600

    Front Page Created

--- 481-app/src/App.js
+++ 481-app/src/App.js
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import COC_Logo from './images/logo-cityofcalgary.png';
 import './App.css';
+import LanguageButton from './components/LanguageButton';
 
 function App() {
   return (
@@ -14,10 +10,16 @@ function App() {
         <img src={COC_Logo} alt="react logo" className="COCLogo"/>
       </header>
       
-      <h1>
-        Select Language
-        
+      <h1 classname = 'WelcomeContainer'>
+        Welcome to Calgary!
       </h1>
+
+      <h2 className='SelectLanguage'>
+        Select Language
+        <div className="LanguageButton"> 
+        <LanguageButton/>
+        </div>
+      </h2>
 
     </div>
   );
---
+++ 481-app/src/components/LanguageButton.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function LanguageButton() {
+  const [language, setLanguage] = React.useState('');
+
+  const handleChange = (event) => {
+    setLanguage(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Language</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={language}
+          label="Language"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>English</MenuItem>
+          <MenuItem value={20}>中国人</MenuItem>
+          <MenuItem value={30}>اردو</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
