7f4417fa535626da3365d3ace50e8f673b2e8d0e
ss1106 <sukriti.sharma@ucalgary.ca>
2024-10-27 02:20:26 -0600

    add base transportation and banking pages✨

--- 481-app/src/App.js
+++ 481-app/src/App.js
@@ -2,29 +2,27 @@ import * as React from 'react';
 import COC_Logo from './images/logo-cityofcalgary.png';
 import './App.css';
 import LanguageButton from './components/LanguageButton';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Banking from './pages/Banking';
+import Transportation from './pages/Transportation';
+
 
 function App() {
   return (
-    
-    <div className="App">
-      <header className="App-header">
-        <img src={COC_Logo} alt="react logo" className="COCLogo"/>
-      </header>
-      
-      <h1 classname = 'WelcomeContainer'>
-        Welcome to Calgary!
-      </h1>
-
-      <h2 className='SelectLanguage'>
-        Select Language
-        <div className="LanguageButton"> 
-        <LanguageButton/>
-        </div>
-      </h2>
-    </div>
-
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={COC_Logo} alt="react logo" className="COCLogo" />
+        </header>
+        
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/banking" element={<Banking />} />
+          <Route path="/transportation" element={<Transportation />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
