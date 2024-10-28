943a3262710921b2baee47df68fe430226f0caf8
FariaMobeen <faria.mobeen@ucalgary.ca>
2024-10-27 21:44:54 -0600

    adjusted routing so each page is rendered separately from language page

--- 481-app/src/App.js
+++ 481-app/src/App.js
@@ -1,30 +1,38 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import COC_Logo from './images/logo-cityofcalgary.png';
 import './App.css';
 import LanguageButton from './components/LanguageButton';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage'; 
+import GovernmentServicesPage from './pages/GovernmentServicesPage'; 
 
 function App() {
   return (
-    
-    <div className="App">
-      <header className="App-header">
-        <img src={COC_Logo} alt="react logo" className="COCLogo"/>
-      </header>
-      
-      <h1 classname = 'WelcomeContainer'>
-        Welcome to Calgary!
-      </h1>
+    <Router>
+      <Routes>
+        {/* Home Route - includes the logo, welcome message, etc. */}
+        <Route path="/" element={
+          <div className="App">
+            <header className="App-header">
+              <img src={COC_Logo} alt="react logo" className="COCLogo"/>
+            </header>
 
-      <h2 className='SelectLanguage'>
-        Select Language
-        <div className="LanguageButton"> 
-        <LanguageButton/>
-        </div>
-      </h2>
-    </div>
+            <h1 className='WelcomeContainer'>
+              Welcome to Calgary!
+            </h1>
 
+            <h2 className='SelectLanguage'>
+              Select Language
+              <div className="LanguageButton"> 
+                <LanguageButton/>
+              </div>
+            </h2>
+          </div>
+        } />
+
+        <Route path="/government-services" element={<GovernmentServicesPage />} />
+      </Routes>
+    </Router>
   );
 }
 
