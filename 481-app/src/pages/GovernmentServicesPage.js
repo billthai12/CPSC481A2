c91781fb4523bdae08c7d3289140fbc35aac57d0
FariaMobeen <faria.mobeen@ucalgary.ca>
2024-10-27 21:55:22 -0600

    template for government serices and internet phone plan done

---
+++ 481-app/src/pages/GovernmentServicesPage.js
@@ -0,0 +1,13 @@
+// src/pages/GovernmentServicesPage.js
+
+import React from 'react';
+
+function GovernmentServicesPage() {
+    return (
+        <div>
+            <h1>GOVERNMENT SERVICES PAGE</h1>
+        </div>
+    );
+}
+
+export default GovernmentServicesPage;
---
+++ 481-app/src/pages/InternetPhonePlanPage.js
@@ -0,0 +1,13 @@
+// src/pages/InternetPhonePlanPage.js
+import React from 'react';
+
+function InternetPhonePlanPage() {
+    return (
+        <div>
+            <h1>Internet Phone Plan</h1>
+            <p>Information about internet phone plans offered in Calgary.</p>
+        </div>
+    );
+}
+
+export default InternetPhonePlanPage;
