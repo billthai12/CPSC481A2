a99195574c0913b986ce697a50736192c4b1e96f
FariaMobeen <faria.mobeen@ucalgary.ca>
2024-10-29 12:02:11 -0600

    app.js from main

--- 481-app/src/App.js
+++ 481-app/src/App.js
@@ -2,11 +2,18 @@ import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import COC_Logo from './images/logo-cityofcalgary.png';
 import './App.css';
-import LanguageButton from './components/LanguageButton';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import LanguageButton from './components/LanguageButton';
 import HomePage from './pages/HomePage';
 import Banking from './pages/Banking';
 import Transportation from './pages/Transportation';
+import LanguagePage from './pages/LanguagePage';
+import EmergencyServices from './pages/EmergencyServices';
+import Checklist from './pages/Checklist';
+import Map from './pages/Map';
+import Internet from './pages/Internet';
+import GovernmentServicesPage from './pages/GovernmentServicesPage';
+import Resources from './pages/Resources';
+import Shopping from './pages/Shopping';
 
 
 function App() {
@@ -18,9 +25,17 @@ function App() {
         </header>
         
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LanguagePage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/banking" element={<Banking />} />
           <Route path="/transportation" element={<Transportation />} />
+          <Route path="/emergencyservices" element={<EmergencyServices />} />
+          <Route path="/checklist" element={<Checklist />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/internet" element={<Internet />} />
+          <Route path="/government-services" element={<GovernmentServicesPage />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/shopping" element={<Shopping />} />
         </Routes>
       </div>
     </Router>
@@ -28,3 +43,4 @@ function App() {
 }
 
 export default App;
+
