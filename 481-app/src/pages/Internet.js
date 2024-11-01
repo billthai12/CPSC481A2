9f3844733fba5aae802c9bb69d4d627b61f8df5b
billthai12 <billthai12@gmail.com>
2024-11-01 13:07:14 -0600

    Fixed header issues in internet and government services page

--- 481-app/src/pages/Internet.js
+++ 481-app/src/pages/Internet.js
@@ -6,7 +6,6 @@ import Header from '../components/Header'; // Import the Header component
 function Internet() {
     return (
         <div>
-                       <Header />
             <h1>Internet Phone Plan</h1>
             <p>Information about internet phone plans offered in Calgary.</p>
         </div>
