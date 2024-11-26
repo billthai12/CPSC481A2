7471599a5c3dbc77fe32a2e7bd10ea1d1be1e322
billthai12 <billthai12@gmail.com>
2024-11-26 10:43:11 -0700

    Revert "Changes"
    This reverts commit f7f10dd975b31b1cb334b893e25c931ea57bf7db.

--- 481-app/src/pages/HomePage.js
+++ 481-app/src/pages/HomePage.js
@@ -13,7 +13,6 @@ import Emergency from '../images/emergency.png';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function HomePage() {
   const navigate = useNavigate();
@@ -22,11 +21,7 @@ function HomePage() {
   return (
     <>
       <div className='HomePageTitle'>
-      <Button 
-      className="BackButton"
-      variant="danger"
-      size="lg"
-      onClick={() => navigate('/')}><i class="bi bi-arrow-left"></i> Back</Button>
+      <Button className="BackButton" variant="danger" size="lg" onClick={() => navigate('/')}>Back to Language Select</Button>
         <h1>Home Page</h1>
       </div>
 
